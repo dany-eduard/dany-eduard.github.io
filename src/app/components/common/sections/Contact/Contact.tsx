da3692f2753dadae632b37beb3aed229525044e1
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-12-18 13:11:24 -0500

    create: constact secction

---
+++ src/app/components/common/sections/Contact/Contact.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import SectionWrapper from '../../../layouts/SectionWrapper/SectionWrapper';
+import { Paragraph } from '../../shared';
+
+const Greeting = styled.div`
+  font-size: clamp(var(--fz-xs), 8vw, var(--fz-xl));
+  font-weight: 400;
+  font-family: var(--code-fonts);
+  color: var(--secondary-color);
+  margin-bottom: 0.8rem;
+`;
+
+const SubTitle = styled.div`
+  > h3 {
+    font-size: clamp(25px, 8vw, 60px);
+    font-weight: 600;
+    font-family: var(--primary-fonts);
+    color: var(--primary-color);
+    line-height: 1.1;
+    margin-left: -0.3rem;
+  }
+`;
+
+const Contact = () => {
+  return (
+    <SectionWrapper>
+      <Greeting>¿Qué es lo siguiente?</Greeting>
+      <SubTitle>
+        <h3 id="contact">Contactame...</h3>
+      </SubTitle>
+      <Paragraph maxWidth="632px">
+        Aunque actualmente no estoy buscando nuevas oportunidades, mi bandeja de entrada está
+        siempre abierta. Tanto si tienes una pregunta como si sólo quieres saludar, haré lo posible
+        por responderte.
+      </Paragraph>
+
+      {/* Diga hola */}
+    </SectionWrapper>
+  );
+};
+
+export default Contact;
--- src/app/components/common/sections/index.tsx
+++ src/app/components/common/sections/index.tsx
@@ -1,3 +1,5 @@
 export { default as SectionIntro } from './Intro/Intro';
 export { default as SectionAbout } from './About/About';
 export { default as SectionExperience } from './Experience/Experience';
+export { default as SectionProjects } from './Projects/Projects';
+export { default as SectionContact } from './Contact/Contact';
