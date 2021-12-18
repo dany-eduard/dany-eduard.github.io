3b1fdab9be54ab21929d5ae2d96c9756cec43390
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-12-17 21:11:24 -0500

    create: section wrapper

---
+++ src/app/components/layouts/SectionWrapper/SectionWrapper.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const SectionWrapper = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+  padding: 0;
+  transition: all 0.5s ease;
+`;
+
+export default SectionWrapper;
