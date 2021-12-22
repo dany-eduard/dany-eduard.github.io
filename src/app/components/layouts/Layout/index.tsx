e2df888e3b5a3e999ec88bfb3bb1fdd3cd0da188
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-12-21 23:06:52 -0500

    add: application skeleton layout

---
+++ src/app/components/layouts/Layout/index.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  display: grid;
+  gird-auto-columns: 1fr;
+  grid-template-columns: 0.2fr 1.8fr;
+  grid-template-rows: 0.2fr 1.8fr;
+  grid-template-areas:
+    'nav nav'
+    'contact-bottons main';
+  max-height: 100vh;
+  overflow-y: hidden;
+
+  @media (max-width: 960px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 0.2fr 1.8fr;
+    grid-template-areas:
+      'nav'
+      'main'
+      'contact-bottons';
+  }
+`;
+
+export const NavBarLayout = styled.div`
+  grid-area: nav;
+`;
+
+export const ContactBottons = styled.div`
+  grid-area: contact-bottons;
+`;
+
+export const Main = styled.main`
+  grid-area: main;
+  max-width: 1366px;
+  padding: 0px 100px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+
+  @media (max-width: 960px) {
+    padding: 0px 10%;
+  }
+`;
+
+export const FooterLayout = styled.div``;
---
+++ src/app/components/layouts/index.tsx
@@ -0,0 +1,2 @@
+export { Layout, NavBarLayout, ContactBottons, Main, FooterLayout } from './Layout';
+export { default as SectionWrapper } from './SectionWrapper';
