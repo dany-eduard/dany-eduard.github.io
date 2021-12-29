91fe63d8f63fe38e3e8a577dc092c65602b2cbce
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-12-28 22:24:58 -0500

    add: contact buttons in the contact section

--- src/app/components/common/sections/Contact/index.tsx
+++ src/app/components/common/sections/Contact/index.tsx
@@ -20,6 +20,32 @@ const SubTitle = styled.div`
   }
 `;
 
+const ContainerLinks = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3rem;
+  margin-bottom: 1rem;
+`;
+
+const ContactLink = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  color: var(--secondary-color);
+  font-size: clamp(var(--fz-xxs), 8vw, var(--fz-lg));
+  font-weight: 500;
+  border: 3px solid var(--primary-color-claro);
+  border-radius: 3px;
+  padding: 0.7rem 1.2rem;
+  margin-right: 5px;
+  cursor: pointer;
+  transition: 0.5s all ease;
+
+  &:hover {
+    background-color: var(--primary-color-gradient);
+  }
+`;
+
 const Contact = () => {
   return (
     <SectionWrapper id="contact">
@@ -32,8 +58,15 @@ const Contact = () => {
         siempre abierta. Tanto si tienes una pregunta como si sólo quieres saludar, haré lo posible
         por responderte.
       </Paragraph>
-
-      {/* Diga hola */}
+      <ContainerLinks>
+        <ContactLink href="mailto:danyeduard17@gmail.com" target="_blank" rel="noopener noreferrer">
+          Di hola en Gmail
+        </ContactLink>
+        <ContactLink href="tel:+573017496696">Llamame</ContactLink>
+        <ContactLink href="https://wa.link/1bdf5b" target="_blank" rel="noopener noreferrer">
+          WhatsApp
+        </ContactLink>
+      </ContainerLinks>
     </SectionWrapper>
   );
 };
